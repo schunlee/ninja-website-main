1978d6647a2bfd152e59ec46dd3c691e6543d162
schunlee <bill.lee.1018@163.com>
2024-10-17 22:29:49 +0800

    Update Banner.tsx

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,5 +1,5 @@
 
-import { Box, Container, Image, SimpleGrid, Stack } from '@chakra-ui/react';
+import { Box, Image, SimpleGrid, Stack } from '@chakra-ui/react';
 import React from 'react';
 
 const Banner = () => {
