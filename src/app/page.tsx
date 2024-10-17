f8209ee59b640ec9c8a68089f238bf875ee3506c
schunlee <bill.lee.1018@163.com>
2024-10-17 22:32:12 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 // app/page.tsx
 'use client'
 import Banner from './components/Banner'
-#import Carousel from './components/Carousel'
 import DownloadApk from './components/DownloadApk'
 import GamerComments from './components/GamerComments'
 
