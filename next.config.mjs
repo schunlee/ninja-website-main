2295dac5c2a2de986c8e2da80165b25d7d1ecf9f
schunlee <xun.lee@gmail.com>
2025-01-20 15:18:09 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true, // 在构建期间忽略 ESLint 错误
-    },
-    typescript: {
-        ignoreBuildErrors: true, // 在构建期间忽略 TypeScript 错误
-    },
-    reactStrictMode: false
-  };
-  
-  export default nextConfig;
+  async rewrites() {
+    return [
+      {
+        source: "/nativetrack", // 不带后缀的路径
+        destination: "/nativetrack.html", // 实际的文件路径
+      },
+    ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // 在构建期间忽略 ESLint 错误
+  },
+  typescript: {
+    ignoreBuildErrors: true, // 在构建期间忽略 TypeScript 错误
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
